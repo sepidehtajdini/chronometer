ce55b7d4edf95ad57406cf8b565eeb7e87d5d9a5
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2023-05-20 18:20:58 +0330

    Delete Main directory

--- Main/Button/Button.js
+++
@@ -1,7 +0,0 @@
-import "./Button.css";
-function Button({ text, className , onClick }) {
-    return (
-        <button onClick={onClick} className={className}>{text}</button>
-    )
-}
-export default Button
